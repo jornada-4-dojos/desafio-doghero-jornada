1fd68da153262b9bad0c9a6d00952d32ee8ee6e5
Alberto - Dev Eficiente <alberto@deveficiente.com>
2021-04-22 20:09:08 -0300

    versao inicial do projeto

---
+++ src/main/java/com/deveficiente/desafiodogherojornada/DesafioDogheroJornadaApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.desafiodogherojornada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDogheroJornadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDogheroJornadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deveficiente/desafiodogherojornada/DesafioDogheroJornadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deveficiente.desafiodogherojornada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDogheroJornadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
